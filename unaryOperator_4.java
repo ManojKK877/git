20107218ff62c304dc2a5a364c2ed7572f6b13f3
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 21:29:09 +0530

    unaryOperator_4 commit

---
+++ unaryOperator_4.java
@@ -0,0 +1,11 @@
+
+public class unaryOperator_4{ 
+public static void main(String[] args)
+    { 
+        int a=28;
+         a+=a++ + ++a + --a + a--; 
+        System.out.println(a);
+
+    }
+
+}
