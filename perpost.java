156636c967d9656ae907e9fa86b6e2d48b990537
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 14:21:51 +0530

    prepostfix commit

---
+++ perpost.java
@@ -0,0 +1,14 @@
+//Write a program to find the difference between prefix and postfix operator
+public class perpost{
+public static void main(String[] args)
+{
+    int a=5,b=5;
+    //a++postfix
+    //++apreficx
+    System.out.println("the postfix value is :"+(a++));
+    System.out.println("the prefix value is :"+(++b));
+
+}
+
+
+}
