2864583b10bac5c742b96e9ddfadfcbe46e375f7
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 20:00:58 +0530

    leapYear commit

---
+++ leapYear.java
@@ -0,0 +1,18 @@
+//find the leap year
+import java.util.Scanner;
+public class leapYear {
+    public  static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        if(year%4==0 && year%200==0 && year%200==0)
+       {
+           System.out.println("this is leap year :"+year);
+       }
+         else
+            System.out.println("this is  Not leap year :"+year);
+    }
+    
+
+    
+}
