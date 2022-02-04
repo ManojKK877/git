c7191acb72b0fcc8191ba84492d19ccaf4a227b3
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 19:38:37 +0530

    unaryOperator_3commit

---
+++ unaryOperator_3.java
@@ -0,0 +1,10 @@
+public class unaryOperator_3 {
+    public static void main(String[] args)
+    { 
+        int x=5;
+        int x1=++x - x++ + --x;
+        System.out.println(x1);
+
+    }
+    
+}
