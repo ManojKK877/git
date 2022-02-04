1c95ce461c6df95f5d9b52e0b7ce766932a43e52
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 14:52:54 +0530

    bitwise commit

---
+++ bitwiseOperator.java
@@ -0,0 +1,20 @@
+// Write a program with bitwise operators
+public class bitwiseOperator {
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=4;
+        
+        System.out.println("the first Method is ARE Operator :"+(a|b));
+        
+        System.out.println("the first Method is AND Operator :" + (a&b));
+
+        System.out.println("the first Method is XOR Operator :"+(a^b));
+
+        System.out.println("the first Method is leftShift Operator :"+(a<<b));
+        
+        System.out.println("the first Method is RightShift Operator :"+(a>>b));
+       
+
+    }
+}
