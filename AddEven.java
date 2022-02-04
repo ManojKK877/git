0609e0bebe76313a9b32255696d748a8090ced91
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 21:46:13 +0530

    AddEvenNumber commit

---
+++ AddEven.java
@@ -0,0 +1,14 @@
+//.Program to Check Whether a Number is Even or Od
+import java.util.Scanner;
+
+public class AddEven {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);       
+        System.out.println("enter the n value :");
+        int n=scan.nextInt();
+        String values = n%2!=0?"ADDnumber":"EvenNumber";
+        System.out.println("this is number is :" +values);
+    }
+    
+}
