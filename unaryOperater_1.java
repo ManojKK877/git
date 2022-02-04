ee8be770d1c67210385e654d8827444c0b94eecf
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 19:23:42 +0530

    unaryOperator  commit

---
+++ unaryOperater_1.java
@@ -0,0 +1,10 @@
+
+public class unaryOperater_1 {
+    public static void main(String[] args)
+    {
+        int x =5;
+       int  b = x++ * 2 + 3 * --x;
+       System.out.println(b);
+    
+    }
+}
