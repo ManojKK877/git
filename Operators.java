7952398e0fd1348945a8293c2f45ab0383bb38ea
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-04 16:20:33 +0530

    opre

---
+++ Operators.java
@@ -0,0 +1,14 @@
+//Evaluate the following expression on paper and on program and understand the diiference
+public class Operators {
+    public static void main(String[] args)
+    {
+
+       int a=5,b=4;
+      
+        System.out.println(a%b++  );
+        System.out.println(++a - b-- );
+        System.out.println(a*=b+5 );
+        System.out.println(a>>>b  );
+
+    }
+}
