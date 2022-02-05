1309e882cead035e2b1f7ad7cb6421c5365ffb52
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-05 13:44:57 +0530

    Alphabet  Character

---
+++ Alphabet.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Alphabet {
+    
+    public static void main(String[] main)
+    {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("enetr the Character:");
+        String values=scan.nextLine();
+        for(int i=0;i<values.length();i++)
+               
+            {     char value= values.charAt(i);
+                if(value>=65 &&value<=90||value>=97 &&value<=122)
+                {
+                    System.out.print("this is Alphabet :"+value);
+                }
+                else
+                System.out.print("this is  not a Alphabet :"+value);
+            }
+        
+
+
+    }
+}
