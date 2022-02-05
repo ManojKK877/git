48c1ce721482c7953fe62ea860af7dc0db6229e4
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-05 23:31:04 +0530

    Display Characters from A to Z

---
+++ DisplayCharacters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ public class DisplayCharacters {
+    public static void main(String[] args){
+      
+        char characters;
+        for(characters=65;characters<=90;characters++)
+        System.out.println("The Display Characters from A to Z"+ characters);
+
+    }
+    
+}
--- GreatestcCommondivisor.java
+++ GreatestcCommondivisor.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
         int inputValur_1=scan.nextInt();
         int inputValur_2=scan.nextInt();
         //ternary Operator
-                int inputValur_3=inputValur_1 < inputValur_2 ? inputValur_1:inputValur_2;
-        while(inputValur_3>=2)
+                int minimum=inputValur_1 < inputValur_2 ? inputValur_1:inputValur_2;
+        while(minimum>=2)
         {
-        if((inputValur_1%inputValur_3==0) && (inputValur_2%inputValur_3==0))
-                System.out.println("The Greatest Common divisor :"+inputValur_3);
+        if((inputValur_1%minimum==0) && (inputValur_2%minimum==0))
+                System.out.println("The Greatest Common divisor :"+minimum);
 
-        inputValur_3--;
+        minimum--;
         }
     }
 }
