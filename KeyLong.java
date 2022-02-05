e0ab2898848f3ec788082c1a60f6d36cf625e7fb
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-05 09:04:31 +0530

    keylongvalue commit

---
+++ KeyLong.java
@@ -0,0 +1,8 @@
+ 
+ public class KeyLong {
+    public static void main(String[] args)
+    {
+        double largerValue =987655444d;
+          System.out.println("the larger value :"+largerValue);
+    }
+ }
