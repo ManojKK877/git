9459b5d52f3ece8677875f893b9a2111cd94f6a5
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-05 21:52:12 +0530

    Fibonacci Sequence commit

---
+++ FibonacciSequence.java
@@ -0,0 +1,19 @@
+ import java.util.Scanner;
+ public class FibonacciSequence {
+      public static void main(String[] args)
+      {
+          Scanner scan =new Scanner(System.in);
+          System.out.println("enter the n value :");
+          int a=-1,b=1,c;
+          int n=scan.nextInt();
+          for(int i=0;i<=n;i++)
+          {
+          c=a+b;
+          a=b;
+          b=c;
+        
+          System.out.println("The FibonacciSequence value :"+c);
+          }
+      }
+      
+}
