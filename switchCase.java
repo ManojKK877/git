814679696b09bf58742fd693464e8e451c7dd183
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-06 09:57:37 +0530

    switchCase commit

---
+++ switchCase.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ public class switchCase {
+    public static void main(String[] args)
+    {
+        Scanner scan =new Scanner(System.in);
+            System.out.println("Select your choices : ");
+        System.out.println(" 1.Addition\n 2.Subractions\n 3.Mutliplication\n 4.Divison");
+            int choice=scan.nextInt();
+        System.out.println("enter the two values :");
+             int a=scan.nextInt();
+            int b=scan.nextInt(),c;
+        switch(choice)
+        {
+            case 1:c=a+b;
+            System.out.println("the Addition value : "+a +"+"+b+"="+c);
+            break;
+            case 2:c=a-b;
+            System.out.println("the Subractions value : "+a +"-"+b+"="+c);
+            break;
+            case 3:c=a*b;
+            System.out.println("the Mutliplication value :"+a +"*"+b+"=" +c);
+            break;
+            case 4:c=a/b;
+            System.out.println("the Divison value: "+a +"/"+b+"="+c);
+            break;
+        }
+
+
+    }
+}
