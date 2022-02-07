09e212df21bb3b1ad51ce056b217012f55527fab
ManojKumarK <kmanojkumar5717@gmail.com>
2022-02-07 09:18:13 +0530

    Factorial  Number in Recursion

---
+++ FactorialNumberRecursion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class FactorialNumberRecursion {
+    public static void main(String[] args) {
+        System.out.println("the enter number :");
+        Scanner scan=new Scanner(System.in);
+        int num = scan.nextInt();
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
+
